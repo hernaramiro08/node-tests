de6a6f20a1b18ec3bbb71c538a2e7f7d0f50c267
ramiro.hernandez <urcoelmaldito@gmail.com>
2017-08-18 15:02:27 -0500

    Add Express app tests

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+var app = express();
+
+app.get("/", (req, res) => {
+    res.status(200).send("Hello world!");
+});
+
+app.get("/error", (req, res) => {
+    res.status(404).send({
+        error: "Page not found",
+        name: "Todo App v1.0"
+    });
+});
+
+app.get("/users", (req, res) => {
+    res.status(200).send([{
+        name: "Romina",
+        age: 3
+    }, {
+        name: "Jessica",
+        age: 30
+    }]);
+});
+
+app.listen(3000, () => {
+    console.log("The server was started")
+});
+
+module.exports.app = app;
